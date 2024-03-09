4291f4c411fabdc271db9ea5420e18fc73a547ff
kabirpatel1203 <kabirkaushikbhai.patel01@student.csulb.edu>
2024-03-09 15:54:45 -0800

    docs.txt added and made changes in OOP

--- OOP.py
+++ OOP.py
@@ -1,17 +1,44 @@
-class Dog:
+
+# INHERITANCE (Method overriding is included)
+
+class Animal:
+    breath = "it does breath."
+
+    def __init__(self):
+        self.life = "yes"
+        self.breath = "it does not breathe."
+
+    def has_organs(self):
+        l = "Any animal have the stomach."
+        return l
+
+class Dog(Animal):
     def __init__(self, name, age):
+        super().__init__()
         self.n = name
         self.a = age
 
     def bark(self):
-        p = f"Dog {self.n} of age {self.a} is barking."
+        p = f"Dog {self.n} of age {self.a} is barking. It also {self.life} and {self.has_organs()} Also, {Animal.breath} and no {self.breath}"
         print(p)
 
     def walk(self):
         print("Dog {self.n} of age {self.a} is walking.")
         
 
-k = Dog("Tommy", 5)
-k.bark()
-k.a = 7
-k.bark()
+# METHOD OVERLOADING
+        
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def add(self, a, b, c):
+        return a + b + c
+
+# Usage
+calc = Calculator()
+result1 = calc.add(2, 3)        # Calls the first add method
+result2 = calc.add(2, 3, 5)     # Calls the second add method
+
+
+
